38bf7b966c27d1fc8adef89abd98c1ed8f057c8f
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2026-03-20 12:04:05 +0200

    Refactor index.php by removing unnecessary HTML and echo statements while maintaining layout inclusion

--- index.php
+++ index.php
@@ -8,18 +8,3 @@ $seo = [
 
 $page_view = __DIR__ . '/pages/home.php';
 require __DIR__ . '/components/layout.php';
-
-<?php
-echo "ok";
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    gdffdgfd
-</body>
-</html>
